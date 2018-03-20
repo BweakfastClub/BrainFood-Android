f6af7ee141fa2074feb9a752455c98b22cf7a4d0
Rushil Perera <pererarushil1940@gmail.com>
2018-03-19 23:15:59 -0400

    Added listener for EditText

--- app/src/main/java/club/bweakfast/foodora/UiUtil.kt
+++ app/src/main/java/club/bweakfast/foodora/UiUtil.kt
@@ -3,6 +3,10 @@ package club.bweakfast.foodora
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import io.reactivex.Observable
 
 /**
  * Created by silve on 3/2/2018.
@@ -21,4 +25,28 @@ fun AppCompatActivity.showFragment(
     if (addToStack)
         transaction.addToBackStack(name)
     transaction.commit()
+}
+
+fun EditText.listenForChanges(): Observable<String> {
+    var currentQuery = ""
+    return Observable.create {
+        this.addTextChangedListener(object : TextListener() {
+            override fun afterTextChanged(s: Editable) {
+                super.afterTextChanged(s)
+                val query = s.toString()
+                if (currentQuery != query) {
+                    currentQuery = query
+                    it.onNext(query)
+                }
+            }
+        })
+    }
+}
+
+open class TextListener: TextWatcher {
+    override fun afterTextChanged(s: Editable) {}
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, end: Int, count: Int) {}
+
+    override fun onTextChanged(s: CharSequence?, start: Int, end: Int, count: Int) {}
 }
