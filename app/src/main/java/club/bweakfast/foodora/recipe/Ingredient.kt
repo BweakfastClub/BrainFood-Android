65b8f049dfe1f38e1991cf0ad277236aca3086a7
Rushil Perera <pererarushil1940@gmail.com>
2018-04-07 08:13:50 -0400

    Modified models to match new schema

--- app/src/main/java/club/bweakfast/foodora/recipe/Ingredient.kt
+++ app/src/main/java/club/bweakfast/foodora/recipe/Ingredient.kt
@@ -1,7 +1,39 @@
 package club.bweakfast.foodora.recipe
 
-/**
- * Created by silve on 3/5/2018.
- */
+import android.os.Parcel
+import android.os.Parcelable
 
-data class Ingredient(val id: Int, val name: String)
+data class Ingredient(
+    val id: Int,
+    val name: String,
+    val grams: Float,
+    val displayType: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readFloat(),
+        parcel.readString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+        parcel.writeFloat(grams)
+        parcel.writeString(displayType)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Ingredient> {
+        override fun createFromParcel(parcel: Parcel): Ingredient {
+            return Ingredient(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Ingredient?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/club/bweakfast/foodora/recipe/Recipe.kt
+++ app/src/main/java/club/bweakfast/foodora/recipe/Recipe.kt
@@ -1,13 +1,56 @@
 package club.bweakfast.foodora.recipe
 
-/**
- * Created by silve on 3/5/2018.
- */
+import android.os.Parcel
+import android.os.Parcelable
 
 data class Recipe(
     val ingredients: List<Ingredient>,
     val title: String,
-    val nutrition: Map<String, String>,
     val servings: Int,
+    val prepMinutes: Int,
+    val cookMinutes: Int,
+    val readyMinutes: Int,
     val imageURL: String
-)
+) : Parcelable {
+    var id = -1
+    var nutrition = mutableMapOf<String, NutritionValue>()
+
+    constructor(parcel: Parcel) : this(
+        parcel.createTypedArrayList(Ingredient),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString()
+    ) {
+        id = parcel.readInt()
+        parcel.readMap(nutrition, NutritionValue::class.java.classLoader)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeTypedList(ingredients)
+        parcel.writeString(title)
+        parcel.writeInt(servings)
+        parcel.writeInt(prepMinutes)
+        parcel.writeInt(cookMinutes)
+        parcel.writeInt(readyMinutes)
+        parcel.writeString(imageURL)
+        parcel.writeInt(id)
+        parcel.writeMap(nutrition)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Recipe> {
+        override fun createFromParcel(parcel: Parcel): Recipe {
+            return Recipe(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Recipe?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
