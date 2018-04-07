45554685791085c81a3f8496d65590631f21ccb6
Rushil Perera <pererarushil1940@gmail.com>
2018-04-07 08:07:23 -0400

    Opens RecipeActivity after clicking recipe

---
+++ app/src/main/java/club/bweakfast/foodora/Intents.kt
@@ -0,0 +1,5 @@
+package club.bweakfast.foodora
+
+object Intents {
+    const val INTENT_RECIPE_ACTIVITY = "recipe_activity"
+}
--- app/src/main/java/club/bweakfast/foodora/browse/RecipesAdapter.kt
+++ app/src/main/java/club/bweakfast/foodora/browse/RecipesAdapter.kt
@@ -1,20 +1,22 @@
 package club.bweakfast.foodora.browse
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import club.bweakfast.foodora.Intents
 import club.bweakfast.foodora.R
 import club.bweakfast.foodora.recipe.Recipe
+import club.bweakfast.foodora.recipe.RecipeActivity
 import com.facebook.drawee.view.SimpleDraweeView
 
 /**
  * Created by silve on 3/20/2018.
  */
 
-class RecipesAdapter(private val recipes: List<Recipe>) :
-    RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder>() {
+class RecipesAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<RecipesAdapter.RecipeViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recipe, parent, false)
         return RecipeViewHolder(view)
@@ -25,6 +27,11 @@ class RecipesAdapter(private val recipes: List<Recipe>) :
         with(holder) {
             name.text = recipe.title
             image.setImageURI(recipe.imageURL)
+            itemView.setOnClickListener {
+                val intent = Intent(it.context, RecipeActivity::class.java)
+                intent.putExtra(Intents.INTENT_RECIPE_ACTIVITY, recipe)
+                it.context.startActivity(intent)
+            }
         }
     }
 
@@ -32,8 +39,6 @@ class RecipesAdapter(private val recipes: List<Recipe>) :
 
     inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val name: TextView by lazy { itemView.findViewById<TextView>(R.id.name) }
-        val image: SimpleDraweeView by lazy {
-            itemView.findViewById<SimpleDraweeView>(R.id.image)
-        }
+        val image: SimpleDraweeView by lazy { itemView.findViewById<SimpleDraweeView>(R.id.image) }
     }
 }
