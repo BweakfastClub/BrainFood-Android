f963385aa280b9fdcbdc18e9d36cbfd705de0d11
Rushil Perera <pererarushil1940@gmail.com>
2018-09-25 08:47:39 -0400

    Updated MainActivity design including toolbar and bottom navigation

--- app/src/main/java/club/bweakfast/foodora/MainActivity.kt
+++ app/src/main/java/club/bweakfast/foodora/MainActivity.kt
@@ -1,21 +1,33 @@
 package club.bweakfast.foodora
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import club.bweakfast.foodora.browse.BrowseFragment
+import club.bweakfast.foodora.home.HomeFragment
 import club.bweakfast.foodora.search.SearchFragment
+import club.bweakfast.foodora.util.listenForChanges
 import club.bweakfast.foodora.util.showFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CustomToolbarActivity() {
+    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState, R.layout.activity_main)
 
         bottomBar.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.tab_browse -> showFragment(BrowseFragment.newInstance())
-                R.id.tab_search -> showFragment(SearchFragment.newInstance())
+                R.id.tab_home -> {
+                    showFragment(HomeFragment.newInstance())
+                    showSearchBox(false)
+                    message = "Good morning! In the mood for some new breakfast ideas?"
+                }
+                R.id.tab_search -> {
+                    val fragment = SearchFragment.newInstance()
+                    fragment.searchListener = searchBox.listenForChanges()
+                    searchBox.requestFocus()
+                    showFragment(fragment)
+                    showSearchBox(true)
+                }
             }
             true
         }
