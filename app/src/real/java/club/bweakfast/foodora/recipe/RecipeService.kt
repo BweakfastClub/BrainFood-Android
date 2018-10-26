c117e7226905c9497ee7c1f58c969e697c2e66e6
Rushil Perera <pererarushil1940@gmail.com>
2018-10-26 10:48:11 -0400

    Updated liking and unliking to match API

--- app/src/real/java/club/bweakfast/foodora/recipe/RecipeService.kt
+++ app/src/real/java/club/bweakfast/foodora/recipe/RecipeService.kt
@@ -1,24 +1,27 @@
 package club.bweakfast.foodora.recipe
 
+import club.bweakfast.foodora.util.mapResponse
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.HTTP
 import retrofit2.http.POST
 import javax.inject.Inject
 
 class RecipeService @Inject constructor(retrofit: Retrofit) {
     private val api = retrofit.create(RecipeAPI::class.java)
 
-    fun likeRecipe(recipeID: Int) = api.likeRecipe(mapOf("recipeId" to recipeID.toString()))
+    fun likeRecipe(recipeID: Int) = api.likeRecipe(mapOf("recipeIds" to listOf(recipeID))).mapResponse()
 
-    fun unlikeRecipe(recipeID: Int) = api.unlikeRecipe(mapOf("recipeId" to recipeID.toString()))
+    fun unlikeRecipe(recipeID: Int) = api.unlikeRecipe(mapOf("recipeIds" to listOf(recipeID))).mapResponse()
 
     interface RecipeAPI {
-        @POST("/users/likes_recipe")
-        fun likeRecipe(@Body recipeIDJSON: Map<String, String>): Single<Response<Void>>
+        @POST("/users/liked_recipes")
+        fun likeRecipe(@Body recipeIDJSON: Map<String, @JvmSuppressWildcards List<Int>>): Single<Response<Void>>
 
-        @POST("/users/unlikes_recipe")
-        fun unlikeRecipe(@Body recipeIDJSON: Map<String, String>): Single<Response<Void>>
+        @HTTP(method = "DELETE", path = "/users/liked_recipes", hasBody = true)
+        fun unlikeRecipe(@Body recipeIDJSON: Map<String, @JvmSuppressWildcards List<Int>>): Single<Response<Void>>
     }
 }
