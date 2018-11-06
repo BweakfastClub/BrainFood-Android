a5dd0e7ea502b1d0e89ba2cad62ad700edc6d933
Rushil Perera <pererarushil1940@gmail.com>
2018-11-06 15:26:03 -0500

    Added attribute for input pref to be disabled

--- app/src/main/java/club/bweakfast/foodora/setup/SetupInputPreference.kt
+++ app/src/main/java/club/bweakfast/foodora/setup/SetupInputPreference.kt
@@ -7,15 +7,21 @@ import android.text.Editable
 import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.core.content.res.use
 import club.bweakfast.foodora.R
 
 class SetupInputPreference(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Preference(context, attrs, defStyleAttr) {
+    private lateinit var input: EditText
+
     var inputWidth = 0f
     var inputType = 0
+    var inputDisabled = false
+        set(value) {
+            field = value
+            setIsInputDisabled(value)
+        }
 
     private val textWatcher: TextWatcher
 
@@ -26,6 +32,7 @@ class SetupInputPreference(context: Context, attrs: AttributeSet?, defStyleAttr:
         context.theme?.obtainStyledAttributes(attrs, R.styleable.SetupInputPreference, defStyleAttr, 0)?.use {
             inputWidth = it.getDimension(R.styleable.SetupInputPreference_inputWidth, 0f)
             inputType = it.getInt(R.styleable.SetupInputPreference_android_inputType, EditorInfo.TYPE_NULL)
+            inputDisabled = it.getBoolean(R.styleable.SetupInputPreference_android_enabled, false).not()
         }
 
         textWatcher = object : TextWatcher {
@@ -42,10 +49,25 @@ class SetupInputPreference(context: Context, attrs: AttributeSet?, defStyleAttr:
     override fun onBindViewHolder(holder: PreferenceViewHolder?) {
         super.onBindViewHolder(holder)
         holder?.itemView?.findViewById<EditText>(R.id.input)?.apply {
+            input = this
             layoutParams?.width = inputWidth.toInt()
             this.inputType = this@SetupInputPreference.inputType
             this.text = SpannableStringBuilder(getPersistedString(""))
             this.addTextChangedListener(textWatcher)
+            setIsInputDisabled(inputDisabled)
+        }
+    }
+
+    private fun setIsInputDisabled(isDisabled: Boolean) {
+        if (::input.isInitialized) {
+            input.apply {
+                this.isEnabled = !isDisabled
+                if (isDisabled) {
+                    this.inputType = EditorInfo.TYPE_NULL
+                } else {
+                    this.inputType = this@SetupInputPreference.inputType
+                }
+            }
         }
     }
 }
