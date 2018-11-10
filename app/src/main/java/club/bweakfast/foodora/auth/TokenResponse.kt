cc8e9b35b1868250097cd1925e0acf4a157a6354
Rushil Perera <pererarushil1940@gmail.com>
2018-11-10 08:48:06 -0500

    AuthenticationService.register returns a token as well

--- app/src/main/java/club/bweakfast/foodora/auth/TokenResponse.kt
+++ app/src/main/java/club/bweakfast/foodora/auth/TokenResponse.kt
@@ -1,5 +1,5 @@
 package club.bweakfast.foodora.auth
 
-class LoginResponse(private val token: String) {
+class TokenResponse(private val token: String) {
     operator fun component1() = token
 }
--- app/src/real/java/club/bweakfast/foodora/auth/AuthenticationService.kt
+++ app/src/real/java/club/bweakfast/foodora/auth/AuthenticationService.kt
@@ -1,6 +1,7 @@
 package club.bweakfast.foodora.auth
 
 import club.bweakfast.foodora.StorageService
+import club.bweakfast.foodora.util.mapResponse
 import io.reactivex.Single
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -19,19 +20,19 @@ class AuthenticationService @Inject constructor(
     private val api: AuthenticationAPI = retrofit.create(AuthenticationAPI::class.java)
     val isLoggedIn = storageService.isLoggedIn
 
-    fun login(email: String, password: String): Single<Response<LoginResponse>> {
-        return api.login(mapOf("email" to email, "password" to password))
+    fun login(email: String, password: String): Single<TokenResponse> {
+        return api.login(mapOf("email" to email, "password" to password)).mapResponse()
     }
 
-    fun register(name: String, email: String, password: String): Single<Response<Void>> {
-        return api.register(mapOf("name" to name, "email" to email, "password" to password))
+    fun register(name: String, email: String, password: String): Single<TokenResponse> {
+        return api.register(mapOf("name" to name, "email" to email, "password" to password)).mapResponse()
     }
 
     interface AuthenticationAPI {
         @POST("/users/login")
-        fun login(@Body loginJSON: Map<String, String>): Single<Response<LoginResponse>>
+        fun login(@Body loginJSON: Map<String, String>): Single<Response<TokenResponse>>
 
         @POST("/users")
-        fun register(@Body registerJSON: Map<String, String>): Single<Response<Void>>
+        fun register(@Body registerJSON: Map<String, String>): Single<Response<TokenResponse>>
     }
 }
