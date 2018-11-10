0379f22664c43b405a821baf4ed4076374fd89ae
Rushil Perera <pererarushil1940@gmail.com>
2018-11-10 10:21:02 -0500

    Fixed bug where user would be brought back to home screen if they pressed back while setting up

--- app/src/main/java/club/bweakfast/foodora/auth/AuthenticationActivity.kt
+++ app/src/main/java/club/bweakfast/foodora/auth/AuthenticationActivity.kt
@@ -89,7 +89,7 @@ class AuthenticationActivity : AppCompatActivity() {
 
     private fun onRegisterSuccess() {
         Intent(this, SetupInfoActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(this)
             finish()
         }
