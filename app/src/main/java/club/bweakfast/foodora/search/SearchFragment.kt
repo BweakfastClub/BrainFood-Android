76dc7a1501a56a82786e1345d6048225b8f4f6ba
Rushil Perera <pererarushil1940@gmail.com>
2018-11-19 19:30:17 -0500

    Fixed InterruptedIOException occurring when searching for recipes

--- app/src/main/java/club/bweakfast/foodora/search/SearchFragment.kt
+++ app/src/main/java/club/bweakfast/foodora/search/SearchFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import club.bweakfast.foodora.FoodoraApp
 import club.bweakfast.foodora.R
+import club.bweakfast.foodora.recipe.Recipe
 import club.bweakfast.foodora.recipe.RecipesAdapter
 import club.bweakfast.foodora.util.onError
 import io.reactivex.Flowable
@@ -15,7 +16,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_search.*
-import kotlinx.android.synthetic.main.view_section_title.view.*
+import java.io.InterruptedIOException
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
@@ -26,7 +27,7 @@ import javax.inject.Inject
  */
 class SearchFragment : Fragment() {
     private val subscriptions: CompositeDisposable = CompositeDisposable()
-    private var adapter: RecipesAdapter? = null
+    private lateinit var adapter: RecipesAdapter
 
     @Suppress("MemberVisibilityCanBePrivate")
     @Inject
@@ -55,22 +56,30 @@ class SearchFragment : Fragment() {
         subscriptions.add(
             searchListener
                 .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
                 .debounce(250, TimeUnit.MILLISECONDS)
+                .distinctUntilChanged()
                 .flatMapSingle { searchViewModel.search(it) }
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    if (adapter == null)
-                        adapter = RecipesAdapter(it, true)
-                    else
-                        adapter!!.submitList(it)
-
-                    resultsList.recyclerView.adapter = adapter
-                }, ::handleError)
+                .retryWhen { errors ->
+                    errors.flatMap {
+                        if (it is InterruptedIOException) Flowable.just(null)
+                        else Flowable.error(it)
+                    }
+                }
+                .subscribe(::setData, ::handleError)
         )
     }
 
-    private fun handleError(throwable: Throwable) {
-        onError(throwable, requireContext())
+    private fun setData(recipes: List<Recipe>) {
+        if (::adapter.isInitialized.not()) {
+            adapter = RecipesAdapter(recipes, true)
+            resultsList.recyclerView.adapter = adapter
+        } else adapter.submitList(recipes)
+    }
+
+    private fun handleError(error: Throwable) {
+        onError(error, requireContext())
     }
 
     companion object {
