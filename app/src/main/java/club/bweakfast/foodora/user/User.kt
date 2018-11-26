f713e8202913d8b28497cc769e33b9675a9b5f88
Rushil Perera <pererarushil1940@gmail.com>
2018-11-26 03:07:11 -0500

    Loads user info from server now

--- app/src/main/java/club/bweakfast/foodora/user/User.kt
+++ app/src/main/java/club/bweakfast/foodora/user/User.kt
@@ -1,9 +1,13 @@
 package club.bweakfast.foodora.user
 
+import club.bweakfast.foodora.recipe.Recipe
+
 /**
  * Created by silve on 3/2/2018.
  */
 
 data class User(val username: String) {
     var password = ""
+    val likedRecipes: List<Recipe>? = emptyList()
+    val mealPlan: Map<String, List<Recipe>>? = emptyMap()
 }
--- app/src/main/java/club/bweakfast/foodora/user/UserViewModel.kt
+++ app/src/main/java/club/bweakfast/foodora/user/UserViewModel.kt
@@ -2,7 +2,9 @@ package club.bweakfast.foodora.user
 
 import club.bweakfast.foodora.StorageService
 import club.bweakfast.foodora.auth.AuthenticationService
+import club.bweakfast.foodora.recipe.RecipeDao
 import io.reactivex.Completable
+import io.reactivex.Single
 import javax.inject.Inject
 
 /**
@@ -12,7 +14,8 @@ import javax.inject.Inject
 class UserViewModel @Inject constructor(
     private val authService: AuthenticationService,
     private val userService: UserService,
-    private val storageService: StorageService
+    private val storageService: StorageService,
+    private val recipeDao: RecipeDao
 ) {
     val isLoggedIn = authService.isLoggedIn
 
@@ -32,7 +35,22 @@ class UserViewModel @Inject constructor(
             }
     }
 
-    fun getUserInfo() = userService.getUserInfo()
+    fun getUserInfo(): Single<User> {
+        return userService.getUserInfo().flatMap { user ->
+            val dbOperations = mutableListOf<Completable>()
+
+            user.likedRecipes?.let { dbOperations.addAll(it.map { recipeDao.addRecipe(it).andThen(recipeDao.addLikedRecipe(it.id)) }) }
+            user.mealPlan?.let {
+                dbOperations.addAll(it.flatMap { (categoryName, recipes) ->
+                    recipes.map {
+                        recipeDao.addRecipe(it).andThen(recipeDao.addRecipeToMealPlan(it.id, listOf(categoryName)))
+                    }
+                })
+            }
+            Completable.merge(dbOperations)
+                .andThen(Single.just(user))
+        }
+    }
 
     fun updateUserInfo(userInfoJSON: Map<String, String>) = userService.updateUserInfo(userInfoJSON)
 }
