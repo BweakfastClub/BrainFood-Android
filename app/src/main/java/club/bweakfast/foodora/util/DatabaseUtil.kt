42c48c03d1a98b97cf489ad69c5f8358c1fe106d
Rushil Perera <pererarushil1940@gmail.com>
2018-11-26 03:08:27 -0500

    Modified map a little, and added new Cursor utility methods

--- app/src/main/java/club/bweakfast/foodora/util/DatabaseUtil.kt
+++ app/src/main/java/club/bweakfast/foodora/util/DatabaseUtil.kt
@@ -3,6 +3,7 @@ package club.bweakfast.foodora.util
 import android.content.ContentValues
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import androidx.core.database.getInt
 
 fun SQLiteDatabase.insert(table: String, contentValues: ContentValues) = insert(table, null, contentValues)
 
@@ -20,17 +21,24 @@ fun SQLiteDatabase.query(
     return query(table, columns, selection, selectionArgs, groupBy, having, orderBy)
 }
 
-inline fun <R> Cursor.map(transform: (Cursor) -> R): List<R> {
-    if (!moveToFirst()) {
-        return emptyList()
-    }
+inline fun <R> Cursor.map(transform: Cursor.() -> R): List<R> {
+    if (!moveToFirst()) return emptyList()
 
     val list = mutableListOf<R>()
     do {
-        list.add(transform(this))
+        list.add(transform())
     } while (moveToNext())
 
     return list
 }
 
+inline fun <R> Cursor.useFirst(block: (Cursor) -> R): R {
+    moveToFirst()
+    return use(block)
+}
+
 fun Boolean.toInt() = if (this) 1 else 0
+
+fun Cursor.getBoolean(columnIndex: Int) = getInt(columnIndex) == 1
+
+fun Cursor.getBoolean(columnName: String) = getInt(columnName) == 1
