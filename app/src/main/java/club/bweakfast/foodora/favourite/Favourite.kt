a12571024752ad22e06289f11a748b0ad40b0a9f
Rushil Perera <pererarushil1940@gmail.com>
2018-11-26 02:41:18 -0500

    Removed favourites as a type

--- app/src/main/java/club/bweakfast/foodora/favourite/Favourite.kt
+++
@@ -1,5 +0,0 @@
-package club.bweakfast.foodora.favourite
-
-import com.google.gson.annotations.SerializedName
-
-abstract class Favourite(@SerializedName("") open val id: Int, val type: FavouriteType)
--- app/src/main/java/club/bweakfast/foodora/favourite/FavouriteType.kt
+++
@@ -1,3 +0,0 @@
-package club.bweakfast.foodora.favourite
-
-enum class FavouriteType { RECIPE }
