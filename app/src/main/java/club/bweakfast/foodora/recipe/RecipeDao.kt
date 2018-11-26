5f1e12b6558eec3e1811ad8b7ea462ce0abeb00f
Rushil Perera <pererarushil1940@gmail.com>
2018-11-26 03:05:19 -0500

    Added methods to retrieve recipe info synchronously

--- app/src/main/java/club/bweakfast/foodora/recipe/RecipeDao.kt
+++ app/src/main/java/club/bweakfast/foodora/recipe/RecipeDao.kt
@@ -5,8 +5,9 @@ import io.reactivex.Single
 
 interface RecipeDao {
     fun getRecipes(recipeIDs: List<Int>? = null): Single<List<Recipe>>
-
+    fun getRecipesList(recipeIDs: List<Int>? = null): List<Recipe>
+    fun getRecipe(recipeID: Int): Single<Recipe>
+    fun getRecipeVal(recipeID: Int): Recipe
     fun addRecipe(recipe: Recipe): Completable
-
     fun removeRecipe(recipe: Recipe): Completable
 }
--- app/src/main/java/club/bweakfast/foodora/recipe/RecipeDaoImpl.kt
+++ app/src/main/java/club/bweakfast/foodora/recipe/RecipeDaoImpl.kt
@@ -1,6 +1,11 @@
 package club.bweakfast.foodora.recipe
 
+import android.database.sqlite.SQLiteConstraintException
 import androidx.core.content.contentValuesOf
+import androidx.core.database.getInt
+import androidx.core.database.getString
+import androidx.core.database.sqlite.transaction
+import club.bweakfast.foodora.db.COLUMN_CATEGORY_NAME
 import club.bweakfast.foodora.db.COLUMN_RECIPE_COOK_MINS
 import club.bweakfast.foodora.db.COLUMN_RECIPE_ID
 import club.bweakfast.foodora.db.COLUMN_RECIPE_IMG_URL
@@ -8,31 +13,62 @@ import club.bweakfast.foodora.db.COLUMN_RECIPE_PREP_MINS
 import club.bweakfast.foodora.db.COLUMN_RECIPE_READY_MINS
 import club.bweakfast.foodora.db.COLUMN_RECIPE_SERVINGS
 import club.bweakfast.foodora.db.COLUMN_RECIPE_TITLE
+import club.bweakfast.foodora.db.COLUMN_REL_RECIPE_ID
 import club.bweakfast.foodora.db.FoodoraDB
+import club.bweakfast.foodora.db.TABLE_LIKED_RECIPES_NAME
+import club.bweakfast.foodora.db.TABLE_MEAL_PLAN_NAME
 import club.bweakfast.foodora.db.TABLE_RECIPE_NAME
-import club.bweakfast.foodora.util.insert
+import club.bweakfast.foodora.db.createRecipeFromCursor
+import club.bweakfast.foodora.recipe.ingredient.IngredientDao
+import club.bweakfast.foodora.recipe.nutrition.NutritionDao
+import club.bweakfast.foodora.util.insertOrThrow
 import club.bweakfast.foodora.util.map
 import club.bweakfast.foodora.util.query
+import club.bweakfast.foodora.util.useFirst
 import io.reactivex.Completable
 import io.reactivex.Single
 import javax.inject.Inject
 
-class RecipeDaoImpl @Inject constructor(private val foodoraDB: FoodoraDB) : RecipeDao {
-    override fun getRecipes(recipeIDs: List<Int>?): Single<List<Recipe>> {
-        return Single.create {
-            val db = foodoraDB.readableDatabase
-            val cursor = if (recipeIDs == null) {
-                db.query(TABLE_RECIPE_NAME)
-            } else {
-                db.query(
-                    table = TABLE_RECIPE_NAME,
-                    selection = "$COLUMN_RECIPE_ID IN (${recipeIDs.joinToString(", ") { "?" }})",
-                    selectionArgs = recipeIDs.map { it.toString() }.toTypedArray()
-                )
-            }
+class RecipeDaoImpl @Inject constructor(
+    private val foodoraDB: FoodoraDB,
+    private val ingredientDao: IngredientDao,
+    private val nutritionDao: NutritionDao
+) : RecipeDao {
+    override fun getRecipes(recipeIDs: List<Int>?): Single<List<Recipe>> = Single.create { it.onSuccess(getRecipesList(recipeIDs)) }
 
-            it.onSuccess(cursor.map { Recipe.createFromCursor(it, emptyList()) })
+    override fun getRecipesList(recipeIDs: List<Int>?): List<Recipe> {
+        val db = foodoraDB.readableDatabase
+        val recipeCursor = if (recipeIDs == null) {
+            db.query(TABLE_RECIPE_NAME)
+        } else {
+            db.query(
+                table = TABLE_RECIPE_NAME,
+                selection = "$COLUMN_RECIPE_ID IN (${recipeIDs.joinToString(", ") { "?" }})",
+                selectionArgs = recipeIDs.map { it.toString() }.toTypedArray()
+            )
         }
+
+        return recipeCursor.use {
+            it.map {
+                val recipeID = recipeCursor.getInt(COLUMN_RECIPE_ID)
+                val ingredients = ingredientDao.getIngredientsList(recipeID)
+                val nutrition = nutritionDao.getNutritionList(recipeID)
+
+                createRecipeFromCursor(this, ingredients, nutrition)
+            }
+        }
+    }
+
+    override fun getRecipe(recipeID: Int): Single<Recipe> = Single.create { it.onSuccess(getRecipeVal(recipeID)) }
+
+    override fun getRecipeVal(recipeID: Int): Recipe {
+        val db = foodoraDB.readableDatabase
+        val recipeCursor = db.query(TABLE_RECIPE_NAME, selection = "$COLUMN_RECIPE_ID = ?", selectionArgs = arrayOf(recipeID.toString()))
+
+        val ingredients = ingredientDao.getIngredientsList(recipeID)
+        val nutrition = nutritionDao.getNutritionList(recipeID)
+
+        return recipeCursor.useFirst { createRecipeFromCursor(recipeCursor, ingredients, nutrition) }
     }
 
     override fun addRecipe(recipe: Recipe): Completable {
