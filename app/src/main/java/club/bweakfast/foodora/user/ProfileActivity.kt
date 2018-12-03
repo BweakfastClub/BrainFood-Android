c69a64caf400a7f7978aaef10660593d7eba9a79
Rushil Perera <pererarushil1940@gmail.com>
2018-12-03 12:35:02 -0500

    Fixed profile screen icons

--- app/src/main/java/club/bweakfast/foodora/user/ProfileActivity.kt
+++ app/src/main/java/club/bweakfast/foodora/user/ProfileActivity.kt
@@ -1,17 +1,19 @@
 package club.bweakfast.foodora.user
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import club.bweakfast.foodora.R
 import club.bweakfast.foodora.custom.CustomToolbarActivity
-import club.bweakfast.foodora.getRandomRecipes
 import club.bweakfast.foodora.recipe.RecipesAdapter
+import club.bweakfast.foodora.settings.SettingsActivity
 import club.bweakfast.foodora.util.onError
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.content_profile.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class ProfileActivity : CustomToolbarActivity() {
     private lateinit var disposable: Disposable
@@ -22,6 +24,10 @@ class ProfileActivity : CustomToolbarActivity() {
 
         message = getString(R.string.message_profile)
         title = getString(R.string.title_profile)
+
+        leftIcon.setImageResource(R.drawable.ic_arrow_left)
+        leftIcon.setOnClickListener { onBackPressed() }
+        rightIcon.setOnClickListener { Intent(this, SettingsActivity::class.java).apply { startActivity(this) } }
 
         disposable = recipeViewModel.getFavouriteRecipes()
             .observeOn(AndroidSchedulers.mainThread())
